1320b5c1e3a0a2ac34776add0ef0e1a01def9a4b
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2021-05-19 15:54:20 -0600

    Login y Registro de Empresa
    Frontend

--- empresa/App.js
+++ empresa/App.js
@@ -1,15 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import Registro from './components/registro'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    /*<View style={styles.container}>
        <Image
         style={styles.tinyLogo}
         source={require('./imagenes/Logo.png')}
       />
-    </View>
+      <Text style={styles.text}>Username:</Text>
+      <TextInput style={styles.input}></TextInput>
+      <Text style={styles.text}>Password:</Text>
+      <TextInput style={styles.input}></TextInput>
+      <TouchableOpacity
+      activeOpacity={0.8}>
+        <Text style={styles.login}>LOG IN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+      activeOpacity={0.8}>
+        <Text style={styles.register}>Register</Text>
+      </TouchableOpacity>
+    </View>*/
+    <Registro></Registro>
   );
 }
 
@@ -23,5 +37,35 @@ const styles = StyleSheet.create({
   tinyLogo: {
     width: 75,
     height: 75,
+    marginBottom: 20,
   },
+  text:{
+    color: '#f0f0f0',
+    fontSize: 12,
+    padding:8,
+    marginRight:110,
+  },
+  input:{
+    borderWidth:3,
+    borderColor:'#f0f0f0',
+    borderRadius:50,
+    marginBottom:20,
+  },
+  login:{
+    color:'#f0f0f0',
+    fontFamily:'Arimo',
+    fontSize:14,
+    backgroundColor:'#448DDB',
+    paddingHorizontal: 60,
+    marginTop: 10, 
+    paddingVertical:5,
+    borderRadius: 50,
+  },
+  register:{
+    fontFamily:'Arial Nova',
+    fontStyle:'italic',
+    fontSize:14,
+    color:'#f0f0f0',
+    marginTop:20,
+  }
 });
---
+++ empresa/components/registro.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import {StyleSheet, Image, View, Text, TextInput, TouchableOpacity} from 'react-native';
+
+export default function registro(){
+    return(
+    <View>
+        <View style = {styles.container}>
+            <Image style ={styles.menu} source={require('../imagenes/Menu.png')}></Image>
+            <Text style ={styles.texto}>Chance al Chile</Text>
+        </View>
+        <View style = {styles.container2}>
+            <Text style = {styles.texto2}>Username:</Text>
+            <TextInput multiline numberOfLines={2} style={styles.input}></TextInput>
+            <Text style = {styles.texto2}>Password:</Text>
+            <TextInput multiline numberOfLines={2} style={styles.input}></TextInput>
+            <Text style = {styles.texto2}>Dirección:</Text>
+            <TextInput multiline numberOfLines={2} style={styles.input}></TextInput>
+            <Text style = {styles.texto2}>Empresa</Text>
+            <TextInput multiline numberOfLines={2} style={styles.input}></TextInput>
+            <Text style = {styles.texto2}>Descripción de la empresa</Text>
+            <TextInput multiline numberOfLines={8} style={styles.descripcion}></TextInput>
+
+            <View style={styles.container3}>
+            <TouchableOpacity
+                activeOpacity={0.8}>
+                <Text style={styles.register}>Register</Text>
+            </TouchableOpacity>
+            </View>
+        </View>
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection:'row',
+      backgroundColor: '#1DCC8B',
+      paddingVertical:10,
+    },
+    texto:{
+        color:'#f0f0f0',
+        fontWeight:'bold',
+        fontFamily:'Arimo',
+        marginLeft:20,
+        fontSize:22,
+    },
+    menu:{
+        width:25,
+        height:25,
+        marginLeft:10,
+    },
+    container2:{
+        flexDirection:'column',
+        backgroundColor: '#f0f0f0',
+        height: '92%',
+    }, 
+    input:{
+        borderWidth:2,
+        borderRightColor: '#f0f0f0',
+        borderTopColor: '#f0f0f0',
+        borderLeftColor: '#f0f0f0',
+        borderBottomColor:'#1DCC8B',
+        fontFamily:'Arimo',
+        marginBottom:40,
+        marginLeft: 50,
+        marginRight: 50,
+    },
+    texto2:{
+        color:'black',
+        fontFamily:'Arimo',
+        fontSize:14,
+        marginLeft: 50,
+        marginTop: 22,
+    },
+    descripcion:{
+        backgroundColor:'#D8D8D8',
+        marginLeft: 50,
+        marginRight: 50,
+        marginTop:10,
+        marginBottom:15,
+    },
+    register:{
+        color:'#f0f0f0',
+        fontFamily:'Arail Nova',
+        fontSize:14,
+        backgroundColor:'#448DDB',
+        paddingHorizontal: 60,
+        marginTop: 10, 
+        paddingVertical:5,
+        borderRadius: 50,
+    },
+    container3: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }
+  });
