b26224456627ac3813a56e46ab430517db864dcf
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-05-27 16:45:16 -0600

    Login and register endpoints

--- server/src/controllers/index.controller.js
+++ server/src/controllers/index.controller.js
@@ -23,6 +23,9 @@ const prueba = async (req, res) => {
         .catch((error) => console.log(error));
 }
 
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// LOGIN
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 const login = async (req, res) => {
     const { usuario, contrasena } = req.headers;
     const query = "MATCH (u:usuarios) WHERE u.usuario=$usuario RETURN u AS node";
@@ -33,27 +36,112 @@ const login = async (req, res) => {
                 node = response[0].get('node');
                 if(node.properties.contrasena === contrasena){
                     res.json({
-                        nodes: response[0].get('node'),
+                        nodes: node,
                         login: true,
-                        message: 'Login successful'
+                        message: 'Login successful.'
                     });
                 }else{
                     res.json({
                         login: false,
-                        message: 'Wrong password'
+                        message: 'Wrong password.'
                     });
                 }
             }else{
                 res.json({
                     login: false,
-                    message: "The user doesn't exists"
+                    message: "The user doesn't exists."
                 });
             }
         })
         .catch((error) => console.log(error));
 }
 
+const loginEmpresa = async (req, res) => {
+    const { nombre, contrasena } = req.headers;
+    const query = "MATCH (e:empresas) WHERE e.nombre=$nombre RETURN e AS node";
+    session.run(query, { nombre })
+        .then((result) => {
+            response = result.records;
+            if(response.length === 1){
+                node = response[0].get('node');
+                if(node.properties.contrasena === contrasena){
+                    res.json({
+                        nodes: node,
+                        login: true,
+                        message: 'Login successful.'
+                    });
+                }else{
+                    res.json({
+                        login: false,
+                        message: 'Wrong password.'
+                    });
+                }
+            }else{
+                res.json({
+                    login: false,
+                    message: "The company doesn't exists."
+                });
+            }
+        })
+        .catch((error) => console.log(error));
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// REGISTER
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+const register = async (req, res) => {
+    const { cv, portafolio, foto, correo, bio, contrasena, usuario, telefono } = req.body;
+    const query = "CREATE (u:usuarios {cv:$cv, portafolio:$portafolio, foto:$foto, correo:$correo, bio:$bio, contrasena:$contrasena, usuario:$usuario, telefono:$telefono}) RETURN u AS node";
+    session.run(query, { cv, portafolio, foto, correo, bio, contrasena, usuario, telefono })
+        .then((result) => {
+            response = result.records;
+            if(response.length === 1){
+                node = response[0].get('node');
+                res.json({
+                    nodes: node,
+                    register: true,
+                    message: 'Register successful.'
+                });
+            }else{
+                res.json({
+                    register: false,
+                    message: "Register failed. Please check all the fields."
+                });
+            }
+        })
+        .catch((error) => console.log(error));
+}
+
+const registerEmpresa = async (req, res) => {
+    const { bio, contrasena, foto, nombre, correo } = req.body;
+    const query = "CREATE (e:empresas {foto:$foto, correo:$correo, bio:$bio, contrasena:$contrasena, nombre:$nombre}) RETURN e AS node";
+    session.run(query, { bio, contrasena, foto, nombre, correo })
+        .then((result) => {
+            response = result.records;
+            if(response.length === 1){
+                node = response[0].get('node');
+                res.json({
+                    nodes: node,
+                    register: true,
+                    message: 'Register successful.'
+                });
+            }else{
+                res.json({
+                    register: false,
+                    message: "Register failed. Please check all the fields."
+                });
+            }
+        })
+        .catch((error) => console.log(error));
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// EXPORT MODULE
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 module.exports = {
     prueba,
-    login
+    login, 
+    loginEmpresa, 
+    register, 
+    registerEmpresa
 };
--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 
-const { prueba, login } = require('../controllers/index.controller');
+const { prueba, login, loginEmpresa, register, registerEmpresa } = require('../controllers/index.controller');
 
 router.get('/prueba', prueba)
 router.get('/login', login)
+router.get('/loginEmpresa', loginEmpresa)
+
+router.post('/user', register)
+router.post('/company', registerEmpresa)
 
 module.exports = router;
