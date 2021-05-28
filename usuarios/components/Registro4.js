fe151e835329a734a55bdb5b6a74d68f8ff148c1
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-05-28 11:25:12 -0600

    Passed Variables

--- usuarios/components/Registro4.js
+++ usuarios/components/Registro4.js
@@ -6,38 +6,39 @@ import { NotoSans_400Regular, useFonts, Mukta_400Regular } from "@expo-google-fo
 import Toast from 'react-native-toast-message';
 import StepIndicator from 'react-native-step-indicator';
 
-export default function Registro4({navigation}) {
-    const labels = ["Cuenta","Datos","Profesional","Docs."];
-  const customStyles = {
-    stepIndicatorSize: 25,
-    currentStepIndicatorSize:30,
-    separatorStrokeWidth: 2,
-    currentStepStrokeWidth: 3,
-    stepStrokeCurrentColor: '#1DCC8B',
-    stepStrokeWidth: 3,
-    stepStrokeFinishedColor: '#1DCC8B',
-    stepStrokeUnFinishedColor: '#aaaaaa',
-    separatorFinishedColor: '#1DCC8B',
-    separatorUnFinishedColor: '#aaaaaa',
-    stepIndicatorFinishedColor: '#1DCC8B',
-    stepIndicatorUnFinishedColor: '#ffffff',
-    stepIndicatorCurrentColor: '#ffffff',
-    stepIndicatorLabelFontSize: 13,
-    currentStepIndicatorLabelFontSize: 13,
-    stepIndicatorLabelCurrentColor: '#1DCC8B',
-    stepIndicatorLabelFinishedColor: '#ffffff',
-    stepIndicatorLabelUnFinishedColor: '#aaaaaa',
-    labelColor: '#999999',
-    labelSize: 13,
-    currentStepLabelColor: '#1DCC8B',
-  };
+export default function Registro4({navigation, route}) {
+    const labels = ["Credenciales","Cuenta","Profesional","Docs."];
+    const { phone, password, mail, username, biography, selectedLanguage, tagsArray } = route.params;
+    const customStyles = {
+        stepIndicatorSize: 25,
+        currentStepIndicatorSize:30,
+        separatorStrokeWidth: 2,
+        currentStepStrokeWidth: 3,
+        stepStrokeCurrentColor: '#1DCC8B',
+        stepStrokeWidth: 3,
+        stepStrokeFinishedColor: '#1DCC8B',
+        stepStrokeUnFinishedColor: '#aaaaaa',
+        separatorFinishedColor: '#1DCC8B',
+        separatorUnFinishedColor: '#aaaaaa',
+        stepIndicatorFinishedColor: '#1DCC8B',
+        stepIndicatorUnFinishedColor: '#ffffff',
+        stepIndicatorCurrentColor: '#ffffff',
+        stepIndicatorLabelFontSize: 13,
+        currentStepIndicatorLabelFontSize: 13,
+        stepIndicatorLabelCurrentColor: '#1DCC8B',
+        stepIndicatorLabelFinishedColor: '#ffffff',
+        stepIndicatorLabelUnFinishedColor: '#aaaaaa',
+        labelColor: '#999999',
+        labelSize: 13,
+        currentStepLabelColor: '#1DCC8B',
+    };
 
-  let [fontsLoaded] = useFonts({
-    NotoSans_400Regular,
-    Mukta_400Regular,
-  });
-  
-  const SomeAreasEmptyAlert = () => {
+    let [fontsLoaded] = useFonts({
+        NotoSans_400Regular,
+        Mukta_400Regular,
+    });
+    
+    const SomeAreasEmptyAlert = () => {
         Toast.show({
             text1: 'Falta Informacion',
             text2: 'Procura que todos los campos esten llenos.',
@@ -47,64 +48,65 @@ export default function Registro4({navigation}) {
         });
     }
 
-  return (
-    <View>
-        <View style = {styles.container2}>
-            <TouchableOpacity onPress = {() => navigation.navigate('Registro3')}>
-                <AntDesign name="back" size={27} color="white"/>
-            </TouchableOpacity>
-            <Text style ={styles.topbartext}>Chance al Chile</Text>
-        </View> 
-        <View style = {styles.stepIndicatorContainer}>
-            <StepIndicator
-                customStyles={customStyles}
-                currentPosition={3}
-                labels={labels}
-                stepCount={4}
-            />
-        </View>
-        <View style={[styles.container_r]}>
-            <FontAwesome.Button name="upload" backgroundColor='#F0F0F0'
-                style={{
-                width: 100,
-                height: 30,
-                borderRadius: 6,
-                backgroundColor: '#1CCC8B',
-                alignSelf: 'center',
-                marginTop: 60,
-                }}>
-                CV
-            </FontAwesome.Button>
-            <FontAwesome.Button name="upload" backgroundColor='#F0F0F0'
-                style={{
-                width: 100,
-                height: 30,
-                borderRadius: 6,
-                backgroundColor: '#1CCC8B',
-                alignSelf: 'center',
-                marginTop: 80,
-            }}>
-                Portfolio
-            </FontAwesome.Button>
-            <Button
-                title="Terminar"
-                titleStyle={{
-                    color: '#fff',
-                    fontSize: 15,
-                    fontFamily:'Mukta_400Regular', 
-                }}
-                buttonStyle={{
-                    marginTop: 80,
-                    width: 250,
+    return (
+        <View>
+            <View style = {styles.container2}>
+                <TouchableOpacity onPress = {() => navigation.navigate('Registro3')}>
+                    <AntDesign name="back" size={27} color="white"/>
+                </TouchableOpacity>
+                <Text style ={styles.topbartext}>Chance al Chile</Text>
+            </View> 
+            <View style = {styles.stepIndicatorContainer}>
+                <StepIndicator
+                    customStyles={customStyles}
+                    currentPosition={3}
+                    labels={labels}
+                    stepCount={4}
+                />
+            </View>
+            <View style={[styles.container_r]}>
+                <FontAwesome.Button name="upload" backgroundColor='#F0F0F0'
+                    style={{
+                    width: 100,
                     height: 30,
-                    borderRadius: 6,
-                    backgroundColor: '#448DDB'
-                }}
-                onPress = {async() => navigation.navigate('Login')}
-            />
+                    borderRadius: 20,
+                    backgroundColor: '#1CCC8B',
+                    paddingLeft: 20,
+                    alignSelf: 'center',
+                    marginTop: 60,
+                    }}>
+                    CV
+                </FontAwesome.Button>
+                <FontAwesome.Button name="upload" backgroundColor='#F0F0F0'
+                    style={{
+                    width: 100,
+                    height: 30,
+                    borderRadius: 20,
+                    backgroundColor: '#1CCC8B',
+                    alignSelf: 'center',
+                    marginTop: 80,
+                }}>
+                    Portfolio
+                </FontAwesome.Button>
+                <Button
+                    title="Terminar"
+                    titleStyle={{
+                        color: '#fff',
+                        fontSize: 15,
+                        fontFamily:'Mukta_400Regular', 
+                    }}
+                    buttonStyle={{
+                        marginTop: 80,
+                        width: 250,
+                        height: 30,
+                        borderRadius: 20,
+                        backgroundColor: '#448DDB'
+                    }}
+                    onPress = {async() => navigation.navigate('Login')}
+                />
+            </View>
         </View>
-    </View>
-  );
+    );
 };
 
 const styles = StyleSheet.create({
