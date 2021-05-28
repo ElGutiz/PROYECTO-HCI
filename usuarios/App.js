3badd5b8753de3f053c1d2bfb6d28765b20da504
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-05-28 03:08:24 -0600

    Companies List Screen Created

--- usuarios/App.js
+++ usuarios/App.js
@@ -3,6 +3,7 @@ import Registro1 from './components/Registro1';
 import Registro2 from './components/Registro2';
 import Registro3 from './components/Registro3';
 import Registro4 from './components/Registro4';
+import Lista from './components/listado';
 import Login from './components/Login';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -23,6 +24,7 @@ export default function App() {
         <Stack.Screen name ="Registro2" component={Registro2} />
         <Stack.Screen name ="Registro3" component={Registro3} />
         <Stack.Screen name ="Registro4" component={Registro4} />
+        <Stack.Screen name ="Lista" component={Lista} />
       </Stack.Navigator>
       <Toast ref={(ref) => Toast.setRef(ref)} />
     </NavigationContainer>
